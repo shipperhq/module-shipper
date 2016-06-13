3005964a0f66dd87099eb79dd6d97c362116ebd1
wsagen <gen@webshopapps.com>
2016-06-13 20:45:58 +0800

    SHQ16-1093 physical building detail object

--- src/Model/Carrier/Processor/ShipperMapper.php
+++ src/Model/Carrier/Processor/ShipperMapper.php
@@ -541,14 +541,14 @@ class ShipperMapper
                     }
                     $admin_value= $attribute->setStoreId(0)->getSource()->getOptionText($aValue);
                     $valueString = is_array($admin_value) ? implode('', $admin_value) : $admin_value;
-                    $warehouseDetail = $this->physicalBuildingDetailFactory->create(['name' => $valueString,
+                  /*  $warehouseDetail = $this->physicalBuildingDetailFactory->create(['name' => $valueString,
                         'inventoryCount' => $this->stockHandler->getOriginInventoryCount($valueString,$item, $product),
                         'availabilityDate' => $this->stockHandler->getOriginAvailabilityDate($valueString,$item, $product),
-                        'inStock' => $this->stockHandler->getOriginInstock($valueString,$item, $product)]);
-                 /*   $warehouseDetail = ['name' => $valueString,
+                        'inStock' => $this->stockHandler->getOriginInstock($valueString,$item, $product)]);*/
+                    $warehouseDetail = ['name' => $valueString,
                         'inventoryCount' => $this->stockHandler->getOriginInventoryCount($valueString,$item, $product),
                         'availabilityDate' => $this->stockHandler->getOriginAvailabilityDate($valueString,$item, $product),
-                        'inStock' => $this->stockHandler->getOriginInstock($valueString,$item, $product)];*/
+                        'inStock' => $this->stockHandler->getOriginInstock($valueString,$item, $product)];
                     $details[] = $warehouseDetail;
                 }
             }
@@ -569,14 +569,14 @@ class ShipperMapper
                 foreach($itemLocations as $aValue) {
                     $admin_value= $attribute->setStoreId(0)->getSource()->getOptionText($aValue);
                     $valueString = is_array($admin_value) ? implode('', $admin_value) : $admin_value;
-                   $locationDetail = $this->physicalBuildingDetailFactory->create(['name' => $valueString,
+                /*   $locationDetail = $this->physicalBuildingDetailFactory->create(['name' => $valueString,
                         'inventoryCount' => $this->stockHandler->getLocationInventoryCount($valueString,$item, $product),
                         'availabilityDate' => $this->stockHandler->getLocationAvailabilityDate($valueString,$item, $product),
-                        'inStock' => $this->stockHandler->getLocationInstock($valueString,$item, $product)]);
-                 /*   $locationDetail = ['name' => $valueString,
+                        'inStock' => $this->stockHandler->getLocationInstock($valueString,$item, $product)]); */
+                    $locationDetail = ['name' => $valueString,
                         'inventoryCount' => $this->stockHandler->getLocationInventoryCount($valueString,$item, $product),
                         'availabilityDate' => $this->stockHandler->getLocationAvailabilityDate($valueString,$item, $product),
-                        'inStock' => $this->stockHandler->getLocationInstock($valueString,$item, $product)];*/
+                        'inStock' => $this->stockHandler->getLocationInstock($valueString,$item, $product)];
                     $details[] = $locationDetail;
                 }
             }
